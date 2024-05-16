c2bf53c5ee36b43b1af3c539ab837daa2460dee6
Artem Zinukov <zinykov_a@mail.ru>
2024-05-16 23:26:13 +0300

    Added a feature to send a photo to tg chat

--- tgbot.py
+++ tgbot.py
@@ -1,14 +1,29 @@
+import os
+import random
+import time
 import telegram
-
 from environs import Env
 
-env = Env()
-env.read_env()
+from main import create_parser
 
-bot = telegram.Bot(token=env.str("TG_BOT_TOKEN"))
-updates = bot.get_updates()
-print(updates[-1])
 
-bot.send_document(chat_id=-4271673960,
-                  document="https://api.nasa.gov/EPIC/archive/natural/2024/"
-                           "05/14/png/epic_1b_20240514003633.png?api_key=DEMO_KEY")
+def send_messages():
+    parser = create_parser()
+    parser.add_argument('time', default=14400, help="Введите время задержки отправления фото",
+                        type=int)
+    parser_args = parser.parse_args()
+    document_dir = os.listdir("A:/Курс_DEVMAN/Lesson4_photo_to_tg/Photo_to_tg/images")
+    while document_dir:
+        random.shuffle(document_dir)
+        for image in document_dir:
+            path_to_file = f"A:/Курс_DEVMAN/Lesson4_photo_to_tg/Photo_to_tg/images/{image}"
+            bot.send_document(chat_id=env.str("CHAT_ID"),
+                              document=open(path_to_file, "rb"))
+            time.sleep(parser_args.time)
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    bot = telegram.Bot(token=env.str("TG_BOT_TOKEN"))
+    send_messages()
